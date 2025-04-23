e2547e8af221566f4ce1c05336e7c1257e2047ec
Ariel Thongkham <arielthongkham@Ariels-MacBook-Air-4.local>
2025-04-22 22:15:41 -0400

    Added routes for pages

--- server.py
+++ server.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+@app.route('/challenge')
+def challenge():
+    return render_template('challenge.html')
+
 if __name__ == '__main__':
    app.run(debug = True, port=5001)
